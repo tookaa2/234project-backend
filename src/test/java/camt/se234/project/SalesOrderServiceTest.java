dc61865c39d6e88e7182a8bbe40d42948a89c32b
Unknown <donutzue@gmail.com>
2018-04-26 15:26:31 +0700

    SalesOrderServiceTest part2 - getAverageSaleOrderPrice
    Implement test case for getAverageSaleOrderPrice using mock object containing orders

--- src/test/java/camt/se234/project/SalesOrderServiceTest.java
+++ src/test/java/camt/se234/project/SalesOrderServiceTest.java
@@ -27,7 +27,7 @@ public class SalesOrderServiceTest {
     salesOrderService.setOrderDao(orderDao);
     }
     @Test
-    public void getSaleOrders(){
+    public void getSaleOrdersTest(){
         List<SaleOrder> orders = new ArrayList<>();
         SaleOrder sale1= new SaleOrder(1234L,"E12R78E",new ArrayList<>());
         ///Product list
@@ -47,6 +47,42 @@ public class SalesOrderServiceTest {
 
         when(orderDao.getOrders()).thenReturn(orders);
         assertThat(salesOrderService.getSaleOrders(),is(orders));
+    }
+
+    @Test
+    public void getAverageSaleOrderPriceTest(){
+        List<SaleOrder> orders = new ArrayList<>();
+        SaleOrder sale1= new SaleOrder(1234L,"E12R78E",new ArrayList<>());
+        SaleOrder sale2= new SaleOrder(7564L,"4D8W9F4",new ArrayList<>());
+
+        ///Product list
+        Product pepsi=new Product(114578L,"pepsi578","pepsi","Kind of drink from pepsi company","./img/114578.png",20.00);
+        Product coke=new Product(114878L,"coke878","coke","Kind of drink from coca cola company","./img/114878.png",25.00);
+        Product carrot=new Product(114432L,"carrotkg","carrot","Vegtable imported from USA","./img/114432.png",35.00);
+        ///Transactions
+        SaleTransaction transaction1= new SaleTransaction(0001L,"7DR94E6",sale1,pepsi,5);
+        SaleTransaction transaction2= new SaleTransaction(0002L,"8E9S5F4",sale1,coke,1);
+        SaleTransaction transaction3= new SaleTransaction(0003L,"7E88RQ9",sale1,carrot,3);
+        //------------------------------
+        SaleTransaction transaction4= new SaleTransaction(0004L,"ASD4AS6",sale2,pepsi,20);
+        SaleTransaction transaction5= new SaleTransaction(0005L,"8D8G4E5",sale2,coke,10);
+        SaleTransaction transaction6= new SaleTransaction(0006L,"66W8F8D",sale2,carrot,11);
+        ///Adding all the transactions into sale1
+        sale1.getTransactions().add(transaction1);
+        sale1.getTransactions().add(transaction2);
+        sale1.getTransactions().add(transaction3);
+
+        sale2.getTransactions().add(transaction4);
+        sale2.getTransactions().add(transaction5);
+        sale2.getTransactions().add(transaction6);
+
+        orders.add(sale1);
+        orders.add(sale2);
+
+        when(orderDao.getOrders()).thenReturn(orders);
+        assertThat(salesOrderService.getAverageSaleOrderPrice(),is(632.5));
+
+
     }
 
 }
